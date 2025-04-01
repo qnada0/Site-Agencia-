2bcc8c7bc33dfa6708e0ede34c4ecbed26378bf0
qnada0 <165679964+qnada0@users.noreply.github.com>
2025-03-31 21:02:16 -0300

    Add files via upload

---
+++ Site - Agencia/js/index.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let elements = document.querySelectorAll(".fade-in");
+    let observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("visible");
+            }
+        });
+    });
+
+    elements.forEach(el => observer.observe(el));
+});
